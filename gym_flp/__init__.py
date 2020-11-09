58bf72a13e3f1790987a04156803a6b19dc7de8c
BTKoke <68201226+BTKoke@users.noreply.github.com>
2020-11-09 20:32:00 +0100

    Update __init__.py

--- gym_flp/__init__.py
+++ gym_flp/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 name = 'gym_flp'
 register(id='qap-v1', entry_point='gym_flp.envs:qapEnv')
-register(id='fbs-v0', entry_point='gym_flp.envs:qspEnv')
+register(id='fbs-v0', entry_point='gym_flp.envs:fbsEnv')
 register(id='mip-v0', entry_point='gym_flp.envs:mipEnv')
