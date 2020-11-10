6b59a1bc107840d8d7df486b57343986e23a2046
BTKoke <68201226+BTKoke@users.noreply.github.com>
2020-11-10 22:05:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='gym_flp',
-      version='0.0.2',
+      version='0.0.3',
       install_requires=['gym', 'numpy'],
       packages=find_packages(),
       include_package_data=True)
