fb7f7434b60bae0d54c58f4ef7e3a3398e2d2c15
BTHeinbach <68201226+BTHeinbach@users.noreply.github.com>
2021-03-04 20:22:56 +0100

    Add new setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(name='gym_flp',
-      version='0.0.3',
+      version='0.1.0',
+      description='Implementation of facility layout problems in OpenAI Gym',
+      url='https://github.com/BTHeinbach/gym-flp',
+      author='Benjamin Heinbach',
+      author_email='benjamin.heinbach@uni-siegen.de',
+      license='MIT',
       install_requires=['gym', 'numpy'],
       packages=find_packages(),
       include_package_data=True)
