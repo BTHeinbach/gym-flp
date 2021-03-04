6a9549b7a78de74bfecaa78776af44dc7adaad19
BTHeinbach <68201226+BTHeinbach@users.noreply.github.com>
2021-03-04 20:24:05 +0100

    Add STS to init

--- gym_flp/__init__.py
+++ gym_flp/__init__.py
@@ -1,6 +1,11 @@
 from gym.envs.registration import register
 
 name = 'gym_flp'
-register(id='qap-v1', entry_point='gym_flp.envs:qapEnv')
+register(id='qap-v0',
+         entry_point='gym_flp.envs:qapEnv')
+
 register(id='fbs-v0', entry_point='gym_flp.envs:fbsEnv')
-register(id='mip-v0', entry_point='gym_flp.envs:mipEnv')
+
+register(id='ofp-v0', entry_point='gym_flp.envs:ofpEnv')
+
+register(id='sts-v0', entry_point='gym_flp.envs:stsEnv')
