b7a85855d01f2b6190e6d8a765ec1aa2f2093426
BTHeinbach <68201226+BTHeinbach@users.noreply.github.com>
2021-03-31 21:00:32 +0200

    Update mhc.py

--- gym_flp/rewards/mhc.py
+++ gym_flp/rewards/mhc.py
@@ -5,19 +5,19 @@ class MHC():
     def __init__(self, shape=None, dtype=np.float32):
         self.shape = shape
         
-    def _compute(self, D, F, s):
+    def compute(self, D, F, s):
         # Compute reward for taking action on old state:  
         # Calculate permutation matrix for new state
         P = self.permutationMatrix(s)
         
         #Deduct results from known optimal value 
         #reward = self.best if np.array_equal(fromState, self.opt) else self.best - 0.5*np.trace(np.dot(np.dot(self.F,P), np.dot(self.D,P.T))) #313 is optimal for NEOS n6, needs to be replaced with a dict object
-        transport_intensity = np.dot(np.dot(F,P), np.dot(D,P.T))
-        MHC = 0.5*np.trace(transport_intensity)
+        transport_intensity = np.dot(np.dot(D,P), np.dot(F,P.T))
+        MHC = np.trace(transport_intensity)
                 
         return MHC, transport_intensity
      
-    def compute(self, D, F, s):       
+    def _compute(self, D, F, s):       
         T = np.zeros((len(s),len(s)))
 
         for i in range(len(s)):
