b9bbccd973b6fdb8d220410d661197a1b5dee716
BTHeinbach <68201226+BTHeinbach@users.noreply.github.com>
2021-09-14 10:20:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(name='gym_flp',
-      version='0.1.2',
+      version='0.1.7',
       description='Implementation of facility layout problems in OpenAI Gym',
       url='https://github.com/BTHeinbach/gym-flp',
       author='Benjamin Heinbach',
       author_email='benjamin.heinbach@uni-siegen.de',
       license='MIT',
-      install_requires=['gym', 'numpy', 'anytree', 'pygame'],
+      install_requires=['gym', 'numpy', 'anytree', 'pygame', 'PIL'],
       packages=find_packages(),
       package_data = {
         # If any package contains *.txt or *.rst files, include them:
