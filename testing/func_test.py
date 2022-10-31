f56b1121697e147be80806f002973dbc3daa30a1
Benny <benjamin.heinbach@gmail.com>
2022-10-31 20:53:16 +0100

    changed collision_test to work with masks. ToDo: implement as recursive function

--- testing/func_test.py
+++ testing/func_test.py
@@ -2,20 +2,18 @@ import gym
 import gym_flp
 from stable_baselines3.common.env_util import make_vec_env
 
-#env = gym.make('ofp-v0', instance='P6', mode='rgb_array')
-env = make_vec_env('ofp-v0', env_kwargs={'mode': 'rgb_array', "instance": 'P6'}, n_envs=1)
+env = gym.make('ofp-v0', instance='P6', mode='rgb_array')
+#env = make_vec_env('ofp-v0', env_kwargs={'mode': 'rgb_array', "instance": 'P6'}, n_envs=1)
 
 
 #print(env.action_space)
 
 env.reset()
-s0 = env.get_attr('internal_state')
+#s0 = env.get_attr('internal_state')
 
 a0 = env.action_space.sample()
 
 
 s1, r, d, i = env.step(a0)
-print(s0)
-print(a0)
-print(env.get_attr('internal_state'))
+#print(env.get_attr('internal_state'))
 
