3361aed38ba34398348abb598d4a2252416c46d1
Benny <benjamin.heinbach@gmail.com>
2022-11-01 21:48:16 +0100

    moved testing outside of package

--- testing/unit/test_rescaling.py
+++ testing/unit/test_rescaling.py
@@ -1,13 +1,18 @@
+import pytest
 from gym_flp.util.preprocessing import Spaces
 
 
-def test_rescale():
-    x = 0
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [(0, 15),
+     (-1, 0),
+     (1, 30),
+     (-0.6, 6)]
+)
+def test_rescale(test_input, expected):
     xmin = 0
     xmax = 30
     a = -1
     b = 1
-    s = Spaces()
-    x_ = s.rescale_actions(x=x, x_min=xmin, x_max=xmax, a=a, b=b)
-
-test_rescale()
+    x_ = Spaces().rescale_actions(x=test_input, x_min=xmin, x_max=xmax, a=a, b=b)
+    assert x_ == expected
