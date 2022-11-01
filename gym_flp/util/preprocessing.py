e6634ba0da3b175d87b21bfc66ed45d586830dfb
Benny <benjamin.heinbach@gmail.com>
2022-11-01 21:48:16 +0100

    moved testing outside of package

--- gym_flp/util/preprocessing.py
+++ gym_flp/util/preprocessing.py
@@ -4,3 +4,6 @@ class Spaces():
 
     def rescale_actions(self, a: int, b: int, x_min: int, x_max: int, x: float):
         return (x-a)*(x_max-x_min)/(b-a)+x_min
+
+    def normalize(self, a:int, b:int, x_min: int, x_max: int, x:float):
+        return (b-a)*(x-x_min)/(x_max-x_min)+a
