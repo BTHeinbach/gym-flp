7471c948468d25c0e1ba97097e5a3dfb78685b6c
Benny <benjamin.heinbach@gmail.com>
2022-11-06 21:43:02 +0100

    implemented Box and multi-discrete action spaces

--- gym_flp/testing/testing.py
+++ gym_flp/testing/testing.py
@@ -4,5 +4,6 @@ import gym_flp
 
 env = gym.make('ofp-v0', mode='human', instance='P6', aspace='box')
 env.reset()
-s, r, d, i = env.step(env.action_space.sample())
-print(s,r,d,i)
+for _ in range(10):
+    s, r, d, i = env.step(env.action_space.sample())
+    print(s,r,d,i)
