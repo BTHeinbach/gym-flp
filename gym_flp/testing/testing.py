36e46033bb322068eaef09aa02e13d1e43abc1d2
Shimraz Rabbani <shimraz.asr.92@gmail.com>
2022-11-22 02:17:23 +0100

    testing reward added

--- gym_flp/testing/testing.py
+++ gym_flp/testing/testing.py
@@ -24,55 +24,27 @@ import numpy as np
                    12, 10,  5,  6,
                    21,  2,  4,  6,
                    11, 20,  4,  4,
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-                    2,  2,  3,  5], dtype=np.uint8)),
+                    2,  2,  3,  5], dtype=np.uint8),0),
      (9, np.array([21, 20,  4,  5,
                     2, 20,  8,  9,
                    11, 11,  5,  6,
                    21,  2,  4,  6,
                    11, 20,  4,  4,
-                    2,  2,  3,  5], dtype=np.uint8)),
+                    2,  2,  3,  5], dtype=np.uint8),0),
      (15, np.array([21, 20,  4,  5,
                     2, 20,  8,  9,
                    11, 10,  5,  6,
                    21,  1,  4,  6,
                    11, 20,  4,  4,
-                    2,  2,  3,  5], dtype=np.uint8)),
+                    2,  2,  3,  5], dtype=np.uint8),0),
      (23, np.array([21, 20,  4,  5,
                      2, 20,  8,  9,
                     11, 10,  5,  6,
                     21,  2,  4,  6,
                     11, 20,  4,  4,
-                    2,  1,  3,  5], dtype=np.uint8))])
-def test_discrete(test_action, expected):
-=======
-=======
->>>>>>> Stashed changes
-                    2,  2,  3,  5], dtype=np.uint8),0),
-     (9, np.array([21, 20,  4,  5,
-               2, 20,  8,  9,
-              11, 11,  5,  6,
-              21,  2,  4,  6,
-              11, 20,  4,  4,
-               2,  2,  3,  5], dtype=np.uint8),1),
-        (15, np.array([21, 20,  4,  5,
-                       2, 20,  8,  9,
-                      11, 10,  5,  6,
-                      21,  1,  4,  6,
-                      11, 20,  4,  4,
-                       2,  2,  3,  5], dtype=np.uint8),0),
-         (23, np.array([21, 20,  4,  5,
-                         2, 20,  8,  9,
-                        11, 10,  5,  6,
-                        21,  2,  4,  6,
-                        11, 20,  4,  4,
-                        2,  1,  3,  5], dtype=np.uint8),0)])
+                    2,  1,  3,  5], dtype=np.uint8),0)])
+
 def test_discrete(test_action, expected, reward_expected):
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     env = gym.make('ofp-v0', mode='human', instance='P6', aspace='discrete', multi=False)
     env.reset()
     s, r, d, i = env.step(test_action)
@@ -95,41 +67,19 @@ def test_discrete(test_action, expected, reward_expected):
                                              11, 11,  5,  6,
                                              21,  2,  4,  6,
                                              12, 20,  4,  4,
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-                                              2,  1,  3,  5], dtype=np.uint8)),
-     (np.array([1, 1, 1, 1, 1, 1]), np.array([21, 21,  4,  5,
+                                             2,  1,  3,  5], dtype=np.uint8),1),
+    (np.array([1, 1, 1, 1, 1, 1]), np.array([21, 21,  4,  5,
                                               2, 21,  8,  9,
                                              11, 11,  5,  6,
                                              21,  3,  4,  6,
                                              11, 21,  4,  4,
-                                              2,  3,  3,  5], dtype=np.uint8)),
+                                              2,  3,  3,  5], dtype=np.uint8),0),
      (np.array([2, 2, 2, 2, 2, 2]), np.array([20, 20,  4,  5,
                                               1, 20,  8,  9,
                                              10, 10,  5,  6,
                                              20,  2,  4,  6,
                                              10, 20,  4,  4,
-                                              1,  2,  3,  5], dtype=np.uint8)),
-=======
-=======
->>>>>>> Stashed changes
-                                              2,  1,  3,  5], dtype=np.uint8),1),
-     (np.array([1, 1, 1, 1, 1, 1]), np.array([21, 21,  4,  5,
-                                         2, 21,  8,  9,
-                                        11, 11,  5,  6,
-                                        21,  3,  4,  6,
-                                        11, 21,  4,  4,
-                                         2,  3,  3,  5], dtype=np.uint8),0),
-    (np.array([2, 2, 2, 2, 2, 2]), np.array([20, 20,  4,  5,
-                                             1, 20,  8,  9,
-                                            10, 10,  5,  6,
-                                            20,  2,  4,  6,
-                                            10, 20,  4,  4,
-                                             1,  2,  3,  5], dtype=np.uint8),0),
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+                                              1,  2,  3,  5], dtype=np.uint8),0),
      (np.array([3, 3, 2, 4, 1, 1]), np.array([21, 19,  4,  5,
                                                2, 19,  8,  9,
                                               10, 10,  5,  6,
@@ -172,27 +122,13 @@ def test_multidiscrete(test_action, expected, reward_expected):
                                              21,  2,  4,  6,
                                              11, 20,  4,  4,
                                               2,  2,  3,  5], dtype=np.uint8)),
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     (np.array([-0.59, 0, 0]), np.array([21, 20,  4,  5,
                                                11, 10,  8,  9,
                                               11, 10,  5,  6,
                                               21,  2,  4,  6,
                                               11, 20,  4,  4,
                                                2,  2,  3,  5], dtype=np.uint8)),
-=======
-=======
->>>>>>> Stashed changes
-     (np.array([-0.59, 0, 0]), np.array([21, 20,  4,  5,
-                                           11, 10,  8,  9,
-                                          11, 10,  5,  6,
-                                          21,  2,  4,  6,
-                                          11, 20,  4,  4,
-                                           2,  2,  3,  5], dtype=np.uint8)),
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
     (np.array([-0.6, 0, 0]), np.array([21, 20,  4,  5,
                                                11, 10,  8,  9,
                                               11, 10,  5,  6,
@@ -205,24 +141,13 @@ def test_multidiscrete(test_action, expected, reward_expected):
                                             21,  2,  4,  6,
                                             11, 20,  4,  4,
                                              2,  2,  3,  5], dtype=np.uint8)),
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
       (np.array([0.2, -0.53, -0.14]), np.array([21, 20,  4,  5,
                                                2, 20,  8,  9,
                                               11, 10,  5,  6,
                                               5,  9,  4,  6,
                                               11, 20,  4,  4,
-                                               2,  2,  3,  5], dtype=np.uint8))
-=======
-=======
->>>>>>> Stashed changes
-     (np.array([0.2, -0.53, -0.14]), np.array([21, 20,  4,  5,
-                                              2, 20,  8,  9,
-                                             11, 10,  5,  6,
-                                             5,  9,  4,  6,
-                                             11, 20,  4,  4,
-                                              2,  2,  3,  5], dtype=np.uint8))
->>>>>>> Stashed changes
+                                               2,  2,  3,  5], dtype=np.uint8)),
              ])
 def test_box(test_action, expected):
     env = gym.make('ofp-v0', mode='human', instance='P6', aspace='box', multi=False)
